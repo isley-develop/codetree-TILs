437054c88bd78b09f1a10d9b4e50931fb9151f40
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-05 16:55:27 +0900

    Title: 입력받아 계산, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240205/입력받아 계산/input-calculate.java
@@ -0,0 +1,13 @@
+// Scanner 사용 (1)
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner 사용 (2)
+        Scanner sc = new Scanner(System.in);
+        // 정수 입력받기
+        int a = sc.nextInt() + 2;
+        // 출력
+        System.out.println(a);
+    }
+}
