5a54a8ffd9f56df7d0bd410d2cd30f0cbf1eb6ab
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-05 13:14:21 +0900

    Title: 합을 복사하기, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240205/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 1, b = 2, c = 3;
+
+        // 세 정수의 합을 각 정수에 복사
+        a = b = c = (a + b + c);
+
+        // 출력
+        System.out.print(a + " " + b + " " + c);
+    }
+}
