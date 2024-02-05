b0e9b52f0bbf9ca81afa704f6e04e8d1c462159b
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-05 13:07:46 +0900

    Title: 변수 값 복사하기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240205/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 1, b = 2, c = 3;
+
+        // 변수 값 복사
+        a = b = c;
+
+        // 출력
+        System.out.print(a + " " + b + " " + c);
+
+    }
+}
