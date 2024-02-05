2d0c6c217282e5b46e91399022e7cac07030442f
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-05 12:53:54 +0900

    Title: 변수 값 교체하기 6, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240205/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 2, b = 5;
+        
+        // 두 값 교환
+        int temp = a;
+        a = b;
+        b = temp;
+
+        // 출력
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
