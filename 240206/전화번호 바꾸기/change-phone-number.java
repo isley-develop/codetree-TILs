bbbb9f15fc4844ad34a8c050bf17aa4c35f0b3fe
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-06 23:20:58 +0900

    Title: 전화번호 바꾸기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/전화번호 바꾸기/change-phone-number.java
+++ 240206/전화번호 바꾸기/change-phone-number.java
@@ -6,17 +6,11 @@ public class Main {
         sc.useDelimiter("-");
 
         // 변수 선언 및 입력
-        int temp;
         int a = sc.nextInt();
         int x = sc.nextInt();
         int y = sc.nextInt();
 
-        // 위치 변경
-        temp = x;
-        x = y;
-        y = temp;
-
         // 출력
-        System.out.println("0" + a + "-" + x + "-" + y);
+        System.out.printf("%03d-%d-%d", a, y, x);
     }
 }
