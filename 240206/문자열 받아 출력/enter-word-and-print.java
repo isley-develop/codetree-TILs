46c88e2e3ea9071501477800384c81d9396f2d28
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-06 21:45:13 +0900

    Title: 문자열 받아 출력, Time: 208ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240206/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 문자열 추출
+        String s = sc.next();
+        
+        //출력
+        System.out.println(s);
+    }
+}
