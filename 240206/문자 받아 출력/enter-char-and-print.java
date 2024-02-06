2ce1492c4575f1cc36836461342352ba62be2c64
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-06 21:43:02 +0900

    Title: 문자 받아 출력, Time: 199ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240206/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 입력받은 문자열에서 문자 추출하기
+        char c = sc.next().charAt(0);
+        
+        // 출력
+        System.out.println(c);
+
+    }
+}
