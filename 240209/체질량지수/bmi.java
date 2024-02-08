109fecb5f0d350a9bd0183aa9aefbc893ca9c3b0
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-09 00:07:17 +0900

    Title: 체질량지수, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240209/체질량지수/bmi.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 키
+        int h_cm = sc.nextInt();
+        double h_m;
+        h_m = h_cm / 100.0;
+
+        // 몸무게
+        int w = sc.nextInt(); 
+
+        double bmi;
+        bmi = w / (h_m * h_m);
+
+        if (bmi >= 25) {
+            System.out.println((int)bmi);
+            System.out.println("Obesity");
+        }
+        else if (bmi < 25) {
+            System.out.println("pass");
+        }
+    }
+}
