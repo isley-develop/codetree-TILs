977c5c7de26f72f1daea1bac386b4c35350e985c
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-09 00:38:30 +0900

    Title: 비교 연산, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240209/비교 연산/comparison-operator.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        //a가 b보다 같거나 큰가?
+        if (a >= b) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println("0");
+        }
+
+        //a가 b보다 큰가?
+        if (a > b) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println("0");
+        }
+
+        //b가 a보다 같거나 큰가?
+        if (b >= a) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println("0");
+        }
+
+        //b가 a보다 큰가?
+        if (b > a) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println("0");
+        }
+
+        //a와 b가 같은가?
+        if (a == b) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println("0");
+        }
+
+        //a와 b가 다른가?
+        if (a != b) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println("0");
+        }
+    }
+}
