812e2e8583611f98d82cc1902af08970417f5831
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-11 19:41:37 +0900

    Title: 삼항연산자 2, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240211/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언, 입력
+        int a = sc.nextInt();
+
+        // 출력
+        System.out.println(a == 1 ? "t" : "f");
+    }
+}
