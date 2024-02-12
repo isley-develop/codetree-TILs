348e1a7e81586ea4d77fd9d8c88d6dd7943c92e0
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-12 22:43:32 +0900

    Title: 살 수 있는 물건, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 240212/살 수 있는 물건/things-able-to-buy.java
+++ 240212/살 수 있는 물건/things-able-to-buy.java
@@ -8,10 +8,10 @@ public class Main {
         int n = sc.nextInt(); // 가진 돈
 
         // 출력
-        if (n == 3000) {
+        if (n >= 3000) {
             System.out.println("book");
         }
-        else if (n == 1000) {
+        else if (n >= 1000) {
             System.out.println("mask");
         }
         else {
