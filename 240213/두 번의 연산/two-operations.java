b3049663bac5d11ec8556497b5ceb7ac6aa14659
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-13 23:48:19 +0900

    Title: 두 번의 연산, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240213/두 번의 연산/two-operations.java
+++ 240213/두 번의 연산/two-operations.java
@@ -12,6 +12,9 @@ public class Main {
         if (a % 3 == 0){
             a = a / 3;
             System.out.println(a);
-        }   
+        }
+        else {
+            System.out.println(a);
+        }
     }
 }
