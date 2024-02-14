6f1ddf8ff2c1220c2d80e5c0c0bea99e7eb9d8a8
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-14 22:22:33 +0900

    Title: 범위 안의 수, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240214/범위 안의 수/number-in-range.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언, 입력
+        int a = sc.nextInt();
+
+        // 출력
+        if (a <= 20 && 10 <= a) {
+            System.out.println("yes");
+        }
+        else {
+            System.out.println("no");
+        }
+    }
+}
