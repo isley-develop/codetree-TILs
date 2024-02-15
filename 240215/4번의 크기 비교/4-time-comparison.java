f8c79e3aac31427e927000277dda21c543618897
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-15 21:54:35 +0900

    Title: 4번의 크기 비교, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240215/4번의 크기 비교/4-time-comparison.java
+++ 240215/4번의 크기 비교/4-time-comparison.java
@@ -11,30 +11,10 @@ public class Main {
         int d = sc.nextInt();
         int e = sc.nextInt();
 
-        // 출력
-        if (b < a) {
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }
-        if (c < a) {
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }
-        if (d < a) {
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }
-        if (e < a) {
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }                
+        // 출력 - 삼항연산자 이용
+        System.out.println(b < a ? 1 : 0);
+        System.out.println(c < a ? 1 : 0);
+        System.out.println(d < a ? 1 : 0);
+        System.out.println(e < a ? 1 : 0);        
     }
 }
