3809a17c3f2e60a915867c0eeebe155aec744f45
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-16 23:23:46 +0900

    Title: 윤년인가, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 240216/윤년인가/is-leap-year.java
+++ 240216/윤년인가/is-leap-year.java
@@ -8,7 +8,16 @@ public class Main {
         int y = sc.nextInt();
 
         // 출력
-        if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
+        // 400의 배수라면 윤년이다
+        if (y % 400 == 0) {
+            System.out.println("true");
+        }
+        // 100의 배수이면서 4의 배수이면 윤년이 아니다 
+        else if (y % 100 == 0) {
+            System.out.println("false");
+        }
+        // 4의 배수라면 윤년이다
+        else if (y % 4 == 0) {
             System.out.println("true");
         }
         else {
