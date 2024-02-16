466a10604b9e86799c51e9d0554343dc19462d4d
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-16 22:58:08 +0900

    Title: 윤년인가, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240216/윤년인가/is-leap-year.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언, 입력
+        int y = sc.nextInt();
+
+        // 출력
+        if (y % 4 == 0) {
+            if ((y % 100 == 0 && y % 400 == 0)|| (y & 100) != 0) {
+                System.out.println("true");
+            }
+            else {
+                System.out.println("false");
+            }
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
