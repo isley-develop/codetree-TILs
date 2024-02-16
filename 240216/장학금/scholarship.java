914ffa15f6b8904896bfdfdb1b1d04d1f765b7b6
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-16 21:38:22 +0900

    Title: 장학금, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240216/장학금/scholarship.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언, 입력
+        int a = sc.nextInt(); // 중간
+        int b = sc.nextInt(); // 기말
+
+        // 출력
+        if (a >= 95) {
+            System.out.println(100000);
+        }
+        else if (a >= 90) {
+            System.out.println(50000);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
