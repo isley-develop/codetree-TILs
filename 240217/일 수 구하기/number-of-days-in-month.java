2ddcc8494a3647fca6e666f979d3dcf67fec6bb0
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-17 23:41:25 +0900

    Title: 일 수 구하기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240217/일 수 구하기/number-of-days-in-month.java
+++ 240217/일 수 구하기/number-of-days-in-month.java
@@ -8,16 +8,25 @@ public class Main {
         int n = sc.nextInt();
 
         // 출력
-        if (n != 2) {
-            if (n % 2 == 0 && n != 8) {
-                System.out.println(30);
-            }
-            else {
-                System.out.println(31);
-            }
-        }
-        else {
+        // 윤년이 아닌 해의 2월은 28일까지만 존재
+        if (n == 2) {
             System.out.println(28);
+        }
+        // 1, 3, 5, 7월은 31일까지 존재
+        else if (n % 2 == 1 && n <= 7) {
+            System.out.println(31);
+        }
+        // 8, 10월도 31일까지 존재
+        else if (n % 2 == 0 && n >= 8) {
+            System.out.println(31);
+        }
+        // 4, 6월은 30일까지 존재
+        else if (n % 2 == 0 && n >= 4) {
+            System.out.println(30);
+        }
+        // 그외의 경우 30일까지 존재
+        else {
+            System.out.println(30);
         }
     }
 }
