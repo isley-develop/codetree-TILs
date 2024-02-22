a499f2f152877a33a50e091f99e004a122575c4e
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-22 23:18:32 +0900

    Title: n부터 1까지 숫자 출력, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240222/n부터 1까지 숫자 출력/print-n-to-1.java
+++ 240222/n부터 1까지 숫자 출력/print-n-to-1.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        // 변수 선언
+        // 변수 선언, 입력
         int n = sc.nextInt();
         int i = n;
 
+        // 출력
         while (i >= 1) {
             System.out.print(i + " ");
             i--;
