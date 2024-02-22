03640194adc527dc6027bd2b831f827fc9c071e8
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-22 23:43:17 +0900

    Title: 문자열 출력하기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240222/문자열 출력하기/print-string.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언, 입력
+        int n = sc.nextInt();
+
+        // 출력
+        for (int i = 1; i <= n; i++) {
+            System.out.println("LeebrosCode");
+        }
+    }
+}
