eb2db1a552956747588961904857c9919862de9a
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-22 23:38:02 +0900

    Title: 문자 출력하기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240222/문자 출력하기/print-text.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 입력
+        char n = sc.next().charAt(0);
+
+        // 출력
+        for (int i = 1; i <= 8; i++) {
+            System.out.print(n);
+        }
+    }
+}
