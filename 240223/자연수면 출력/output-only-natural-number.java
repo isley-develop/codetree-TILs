41d4b735244b6f3a1f9a37e8ec4b316700afa62d
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-23 23:50:07 +0900

    Title: 자연수면 출력, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240223/자연수면 출력/output-only-natural-number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언, 입력
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // 출력
+        if (a > 0) {
+            for (int i = 1; i <= b; i++) {
+                System.out.print(a);
+            }
+        }
+        else {
+            System.out.print(0);
+        }
+    }
+}
