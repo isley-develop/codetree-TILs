f667acd247a60c81626639e0050022c287380427
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-23 23:45:15 +0900

    Title: 숫자의 증감, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240223/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언, 입력
+        char c = sc.next().charAt(0);
+        int n = sc.nextInt();
+
+        // 출력
+        if (c == 'A') {
+            for (int i = 1; i <= n; i++) {
+                System.out.print(i + " ");
+            }
+        }
+        else {
+            for (int i = n; i >= 1; i--) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
