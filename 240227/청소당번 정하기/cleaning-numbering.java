aa91afb2eef5896cce740703a76d0c457b6630b1
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-27 23:03:58 +0900

    Title: 청소당번 정하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240227/청소당번 정하기/cleaning-numbering.java
+++ 240227/청소당번 정하기/cleaning-numbering.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        // 변수 선언
+        // 변수 선언, 입력
         int n = sc.nextInt();
+
         int cnt2 = 0, cnt3 = 0, cnt12 = 0;
         // cnt2 교실, cnt3 복도, cnt12 화장실
 
-        // 입력
+        // 날짜별 확인
         for(int i = 1; i <= n; i++) {
 
             if(i % 12 == 0)
