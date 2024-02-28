69e8d7af56b0c699c940bad4eee7555d592f63f3
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-02-29 00:50:42 +0900

    Title: 특정 조건 배수의 합, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240229/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언, 입력
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = 0;
+
+        // a와 b사이의 수 중 5의 배수들의 합
+        for (int i = a; i <= b; i++) {
+            if (i % 5 == 0) {
+                sum += i;
+            }
+        }
+        
+        // 합 출력
+        System.out.print(sum);
+    }
+}
