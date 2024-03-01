a512cd36bd147e5b65450873c9b2dc3f1bf72120
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-01 22:26:24 +0900

    Title: 범위 안의 수 2, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/범위 안의 수 2/number-in-range-2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언 및 입력
+        int n;
+        int sum = 0, cnt = 0;
+        double avg;
+
+        // 0이상 200이하의 정수들의 합
+        for(int i = 1; i <= 10; i++) {
+            n = sc.nextInt();
+
+            if (n >= 0 && n <= 200){
+                sum += n;
+                cnt ++;
+            }
+        }       
+
+        // 평균 구하기
+        avg = (double)sum / cnt;
+
+        // 출력
+        System.out.printf("%d", sum);
+        System.out.print(" ");
+        System.out.printf("%.1f", avg);
+    }
+}
