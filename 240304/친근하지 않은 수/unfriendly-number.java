6c30b1cf1131cc5a1670921c67ec64cd2b048efd
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-04 22:47:35 +0900

    Title: 친근하지 않은 수, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240304/친근하지 않은 수/unfriendly-number.java
+++ 240304/친근하지 않은 수/unfriendly-number.java
@@ -9,15 +9,12 @@ public class Main {
        int cnt = 0;
        
        // 친근한 수
-        int n = sc.nextInt();
-        int cnt = 0;
-
         for(int i = 1; i <= n; i++) {
             if(i % 2 == 0 || i % 3 == 0 || i % 5 == 0)
                 continue;
             cnt++;
         }
-        
+
         // 출력
         System.out.println(cnt);
     }
