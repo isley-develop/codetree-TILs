251a8d858ff6c5f4ed3aaf99678507f81a8272c5
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-08 22:48:31 +0900

    Title: 1까지 나누기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240308/1까지 나누기/divide-by-1.java
+++ 240308/1까지 나누기/divide-by-1.java
@@ -5,14 +5,13 @@ public class Main {
 
         // 변수 선언, 입력
         int n = sc.nextInt();
-        int a = 0, cnt = 0;
+        int cnt = 0;
 
         // 1 이하가 되는 순간까지 나눗셈을 진행한 총 횟수를 구하기
-        for (int i = 1; i <= n; i++) {
-            cnt = i;
-            n =  n / cnt;
-            if (n / i <= 1) {
-                cnt ++;
+        for (int i = 1; i <= 100; i++) {
+            n =  n / i;
+            cnt ++;
+            if (n <= 1) {
                 break;
             }
         }
