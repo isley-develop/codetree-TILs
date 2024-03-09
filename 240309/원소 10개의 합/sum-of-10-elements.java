76331adff861da92f01a846c10ab81cb67b9a1a1
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-09 23:35:03 +0900

    Title: 원소 10개의 합, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240309/원소 10개의 합/sum-of-10-elements.java
+++ 240309/원소 10개의 합/sum-of-10-elements.java
@@ -3,14 +3,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 변수 선언, 입력
-        int val; 
+        // 배열 만들기
+        int[] arr = new int[10];
+
+        // 변수 선언
         int sum = 0; 
 
         // 주어진 10개 원소의 합
         for (int i = 0; i < 10; i++) {
-            val = sc.nextInt();
-            sum += val;
+            arr[i] = sc.nextInt();
+            sum += arr[i];
         }
 
         // 출력
