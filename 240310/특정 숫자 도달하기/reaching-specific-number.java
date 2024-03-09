73a2bf06aff957404fcd1b0e619351ce702b261b
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-10 00:39:09 +0900

    Title: 특정 숫자 도달하기, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240310/특정 숫자 도달하기/reaching-specific-number.java
+++ 240310/특정 숫자 도달하기/reaching-specific-number.java
@@ -6,25 +6,22 @@ public class Main {
         // 배열 만든 뒤 초기값 설정
         int[] arr = new int[10];
 
-        // 합계, 평균 변수 선언
+        // 변수 선언
         int sum = 0;
-        double avg = 0;
         int cnt = 0;
 
-        // 250이상의 정수가 주어진 경우 합을 구한다
-        // 마지막으로 주어진 수는 제외한다
+        // 10개 정수 입력받은 뒤
+        // 250이상의 정수가 주어진 경우 합 구하기
         for (int i = 0; i <= 9; i++) {
             arr[i] = sc.nextInt();
-            cnt ++;
-            if (arr[i] >= 250) {
-                cnt -= 1;
+            if (arr[i] >= 250)
                 break;
-            }
             sum += arr[i];
+            cnt ++;
         }
 
         // 평균 구하기
-        avg = (double) sum / cnt;
+        double avg = (double) sum / cnt;
 
         // 합과 평균 출력하기
         System.out.printf("%d %.1f", sum, avg);
