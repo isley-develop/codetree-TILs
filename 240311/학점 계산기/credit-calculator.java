98869f03a0cfea7b0c8593a4c13cb732e96913d9
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-11 16:58:17 +0900

    Title: 학점 계산기, Time: 171ms, Memory: 12MB, Status: Passed - CodeTree

--- 240311/학점 계산기/credit-calculator.java
+++ 240311/학점 계산기/credit-calculator.java
@@ -10,17 +10,15 @@ public class Main {
         // 크기가 n인 실수형을 저장하기 위한 배열
         double[] arr = new double[n];
         double sum = 0, avg = 0;
-        int cnt = 0;
 
-        // 배열에 n개 과목 학점 입력받고 학점 합계 구하기
+        // 배열에 실수 입력받기, 입력받은 실수 합 구하기
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextDouble();
             sum += arr[i];
-            cnt ++;
         }
 
-        // 평균
-        avg = sum / (double)cnt;
+        // 평균 구하기
+        avg = sum / n;
 
         // 평균학점 출력
         System.out.printf("%.1f\n", avg);
