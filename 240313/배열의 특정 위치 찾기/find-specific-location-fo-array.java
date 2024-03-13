fd21c844a4652d3154ff928447b177afd79b4b72
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-13 21:13:39 +0900

    Title: 배열의 특정 위치 찾기, Time: 175ms, Memory: 10MB, Status: Passed - CodeTree

--- 240313/배열의 특정 위치 찾기/find-specific-location-fo-array.java
+++ 240313/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -6,6 +6,7 @@ public class Main {
 
         // 변수 선언
         int sum1 = 0, sum2 = 0;
+        int cnt = 0;
 
         // 정수 10개 입력받을 배열 생성
         int[] arr = new int[10];
@@ -21,9 +22,10 @@ public class Main {
         // 3의 배수 번째로 입력된 값의 합
         for (int i = 2; i < 10; i += 3) {
             sum2 += arr[i];
+            cnt ++;
         }
         // 평균 구하기
-        double avg2 = (double) sum2 / 3;
+        double avg2 = (double) sum2 / cnt;
 
         // 정수의 합 출력
         System.out.printf("%d %.1f", sum1, avg2);
