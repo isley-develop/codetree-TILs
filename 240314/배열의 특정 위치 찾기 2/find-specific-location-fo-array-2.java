5629345dc05f8806e9ac8481654026f0885d4c1a
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-14 23:59:23 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240314/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 10개 문자 입력받을 배열 생성
+        int[] arr = new int[10];
+
+        // 변수 선언
+        int sum1 = 0, sum2 = 0;
+        int cnt = 0;
+
+        // 정수 입력받아 배열에 저장
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 홀수 번째 입력받은 정수의 합
+        for (int i = 1; i <= 10; i += 2){
+            sum1 += arr[i];
+        }
+
+        // 짝수 번째 입력받은 정수의 합
+        for (int i = 0; i < 10; i += 2) {
+            sum2 += arr[i];
+        }
+
+        // 큰 수에서 작은 수를 뺀 값을 출력
+        if (sum1 > sum2) {
+            System.out.print(sum1 - sum2);
+        }
+        else {
+            System.out.print(sum2 - sum1);
+        }
+    }
+}
