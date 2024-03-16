b83b8918f916a2d498479bc54372ade6dbb8c7a0
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-16 23:32:26 +0900

    Title: 최댓값 구하기, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240316/최댓값 구하기/maximum-value.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언, 입력
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // 조건에 맞는 결과 출력
+        if (a >= b && a > c) {
+            System.out.print(a);
+        }
+        else if (b > a && a > c) {
+            System.out.print(b);
+        }
+        else {
+            System.out.print(c);
+        }
+    }
+}
