9ad24b0cadb62c32e2d60d6a5c22e05b219eb8ec
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-20 22:19:31 +0900

    Title: 100 도달하기, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240320/100 도달하기/reach-100.java
+++ 240320/100 도달하기/reach-100.java
@@ -8,7 +8,7 @@ public class Main {
 
         // 정수 n 입력받기
         int n = sc.nextInt();
-        int cnt1 = 0, cnt2 = 0;
+        int cnt = 1;
 
         // 첫 번째 항은 1로 초기화
         // 정수 n을 두 번째 항으로 초기화
@@ -18,16 +18,14 @@ public class Main {
         // 전전항과 전항을 더한 수가 100을 넘길 때까지 배열 채우기
         for(int i = 2; i < 100; i++) {
             arr[i] = arr[i - 1] + arr[i - 2];
-            cnt1 ++;
+            cnt ++;
 
             if(arr[i] >= 100)
-                cnt2++;
-            if(cnt2 == 1)
                 break;
         }
 
         // 출력
-        for(int i = 0; i <= cnt1 + 1; i++){
+        for(int i = 0; i <= cnt; i++){
             System.out.print(arr[i] + " ");
         }
     }
