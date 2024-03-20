cd1f79d126e79b38c62a74b247e008f553c61218
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-20 21:10:22 +0900

    Title: 숫자들의 배수, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240320/숫자들의 배수/multiple-of-numbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 원소 개수 입력받을 변수 선언
+        int n = sc.nextInt();
+        int cnt_n = 0, cnt_5 = 0;
+
+        // 배열 생성
+        int[] arr = new int[10];
+
+        // 배열에 n개의 원소 입력받기
+        for(int i = 0; i < 10; i++) {
+            arr[i] = (n * (i+1));
+            System.out.print(arr[i] + " ");
+
+            // 5의 배수 카운트
+            if (arr[i] % 5 == 0)
+                cnt_5 ++;
+
+            if (cnt_5 == 2)
+                break;
+        }
+    }
+}
