d35f0ea154f5e188a72acbde1ff93fbf2cfcb815
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-23 23:12:12 +0900

    Title: 3n + 1 수열, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240323/3n + 1 수열/3n-plus-1-sequence.java
+++ 240323/3n + 1 수열/3n-plus-1-sequence.java
@@ -3,22 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언, 입력
         int n = sc.nextInt();
         int cnt = 0;
-        
+
         while (true) {
+
+            // n이 1일 때 중단
+            if (n == 1) {
+                System.out.println(cnt);
+                break;
+            }
+            // n이 짝수일 때
             if (n % 2 == 0) {
                 n = n / 2;
                 cnt++;
             }
+            // n이 홀수일 때
             else {
-                n = n * 3 + 1;
+                n = (n * 3) + 1;
                 cnt++;
-            }
-
-            if (n == 1) {
-                System.out.println(cnt);
-                break;
             }
         }
     }
