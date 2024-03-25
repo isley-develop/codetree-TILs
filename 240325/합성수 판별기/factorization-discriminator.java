cb981882b70f7737baf3c6d91ca0e4080f2ac65a
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-25 21:59:05 +0900

    Title: 합성수 판별기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240325/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        int n = sc.nextInt();
+
+        // boolean type 변수 선언
+        boolean satisfied = false;
+
+        // 조건문
+        for (int i = 2; i <= (n - 1); i++) {
+            if (n % i == 0) {
+                satisfied = true;
+            }
+        }
+
+        // 조건 만족 시 YES 또는 NO 출력
+        if (satisfied == true) {
+            System.out.println("C");
+        }
+        else {
+            System.out.println("N");
+        }
+    }
+}
