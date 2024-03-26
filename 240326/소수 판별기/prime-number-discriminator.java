e6ae2cbb1a30d3596fec44b6207773d46697cef8
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-26 23:35:18 +0900

    Title: 소수 판별기, Time: 251ms, Memory: 10MB, Status: Passed - CodeTree

--- 240326/소수 판별기/prime-number-discriminator.java
+++ 240326/소수 판별기/prime-number-discriminator.java
@@ -10,7 +10,7 @@ public class Main {
 
         for (int i = 2; i <= (n - 1) ; i++) {
             if(n % i == 0) {
-                satisfied == false;
+                satisfied = false;
             }
         }
         
