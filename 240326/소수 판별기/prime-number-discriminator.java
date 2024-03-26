7e1e9683aee8f3bfb1a8b1d6c37e8b923aa22491
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-03-26 09:51:12 +0900

    Title: 소수 판별기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240326/소수 판별기/prime-number-discriminator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언, 입력
+        int n = sc.nextInt();
+        boolean satisfied = true;
+
+        // 조건
+        for (int i = 1; i <= n; i++) {
+            if (i % 1 != 0) {
+                if (i % n != 0) 
+                    satisfied = false;
+            }
+        }
+
+        // 출력
+        if (satisfied == true) {
+            System.out.println("P");
+        }
+        else {
+            System.out.println("C");
+        }
+    }
+}
