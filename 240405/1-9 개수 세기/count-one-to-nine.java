57017b3c205369c72024d9445bb277ec5a7c483b
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-04-05 00:11:08 +0900

    Title: 1-9 개수 세기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240405/1-9 개수 세기/count-one-to-nine.java
+++ 240405/1-9 개수 세기/count-one-to-nine.java
@@ -6,8 +6,8 @@ public class Main {
 
         // 변수 선언, 입력
         int n = sc.nextInt();
-        int[] arr = new int[11];
-        int[] countArr = new int[11];
+        int[] arr = new int[100];
+        int[] countArr = new int[100];
 
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
