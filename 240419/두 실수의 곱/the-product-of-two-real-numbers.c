f01ee4f0c2c577678df6d4d2ad848ee9e8526a45
isley-develop <155369522+isley-develop@users.noreply.github.com>
2024-04-19 00:03:15 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240419/두 실수의 곱/the-product-of-two-real-numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 변수 선언
+    double a = 5.26;
+    double b = 8.27;
+
+    // 두 수의 곱
+    double c = a * b;
+
+    // 출력
+    printf("%.3lf", c);
+
+    return 0;
+}
